b5ebfa4bcb50982bbb3b747ba66eb0344a779956
SpiritAbsolute <spiritabsolute@yandex.ru>
2013-10-01 18:09:12 +0300

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Spirit Absolute"
+    email    "spiritabsolute@yandex.ru"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
