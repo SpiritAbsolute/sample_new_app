8bd689f3f8152abb48d8647bd04431bcb78ff93a
SpiritAbsolute <spiritabsolute@yandex.ru>
2013-10-02 18:46:52 +0300

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,30 +4,35 @@ describe "User pages" do
 
   subject { page }
 
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_selector('h1',    text: user.name) }
+    it { should have_selector('title', text: user.name) }
+  end
+
   describe "signup page" do
     before { visit signup_path }
 
     it { should have_selector('h1',    text: 'Sign up') }
-    it { should have_selector('title', text: 'Sign up') }
-  end
-
-  describe "profile page" do
-  let(:user) { FactoryGirl.create(:user) }
-  before { visit user_path(user) }
-
-  it { should have_selector('h1',    text: user.name) }
-  it { should have_selector('title', text: user.name) }
+    it { should have_selector('title', text: full_title('Sign up')) }
   end
 
   describe "signup" do
 
     before { visit signup_path }
 
-    let(:submit) { "Create my account" }
-
     describe "with invalid information" do
       it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
+        expect { click_button "Create my account" }.not_to change(User, :count)
+      end
+      
+      describe "error messages" do
+        before { click_button "Create my account" }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
       end
     end
 
@@ -40,7 +45,18 @@ describe "User pages" do
       end
 
       it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+        expect do
+          click_button "Create my account"
+        end.to change(User, :count).by(1)
+      end
+
+      describe "after saving the user" do
+        before { click_button "Create my account" }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
   end
